76898b12be60289b3746a0623ac92f57763aaffc
benjaahagen <71642618+benjaahagen@users.noreply.github.com>
2023-07-28 17:13:07 +0200

    Add files via upload

---
+++ Loop structure.py
@@ -0,0 +1,25 @@
+import random
+
+class Class:
+    
+    #Generate a given amount of random numbers between a range of numbers.
+    def random_number_generator(amount, minRange, maxRange):
+
+        assert isinstance(amount, int) #Precondition 1 - amount shold be an integer.
+        assert minRange >= 0 #Precondition 2 - consider only positive numbers.
+        assert minRange < maxRange #Precondition 3 - minRange should be less than maxRange.
+
+        random_numbers = []
+        i = 0
+        while i < amount:
+            random_numbers.append(random.randint(minRange, maxRange))
+            i += 1
+
+        assert len(random_numbers) == amount #Postcondition 1 - there should only be an expected amount of random numbers.
+        assert min(random_numbers) >= minRange #Postcondition 2 - all the random numbers should be greater than or equal to minRange
+        assert max(random_numbers) <= maxRange #Postcondition 3 - all the random numbers should be less than or equal to maxRange.
+
+        return random_numbers
+
+x = Class.random_number_generator(10, 1, 100)
+print (x)
